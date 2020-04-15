2ec0d3c82e5d7b422b82f21d0435271ef444add1
jingxuan <jingxuan.shen-ext@ikang.com>
2020-04-15 12:26:55 +0800

    UI遮挡

---
+++ UGUICase/Assets/Scripts/UIKeepOut/D3Click.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class D3Click : MonoBehaviour
+{
+    private int _index;
+    private GraphicRaycaster _raycaster;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _raycaster = FindObjectOfType<GraphicRaycaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && !IsUI())
+        {
+            ChangeColor();
+        }
+    }
+
+    private bool IsUI()
+    {
+        PointerEventData data = new PointerEventData(EventSystem.current)
+        {
+            pressPosition = Input.mousePosition,
+            position = Input.mousePosition
+        };
+        List<RaycastResult> results = new List<RaycastResult>();
+        _raycaster.Raycast(data, results);
+        return results.Count > 0;
+    }
+
+    void ChangeColor()
+    {
+        if (_index == 0)
+        {
+            GetComponent<MeshRenderer>().material.SetColor("_Color", Color.black);
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().material.SetColor("_Color", Color.white);
+        }
+        _index = _index == 0 ? 1 : 0;
+    }
+}
---
+++ UGUICase/Assets/Scripts/UIKeepOut/UIClick.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIClick : MonoBehaviour,IPointerClickHandler
+{
+    private int _index;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void ChangeColor()
+    {
+        if (_index == 0)
+        {
+            GetComponent<Image>().color = Color.black;
+        }
+        else
+        {
+            GetComponent<Image>().color = Color.white;
+        }
+        _index = _index == 0 ? 1 : 0;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        ChangeColor();
+    }
+}
